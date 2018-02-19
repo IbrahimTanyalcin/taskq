fc54b26f64492b83eb581cc4d9e40a7af91381d5
ibrahim <ibrahim.tanyalcin@i-pv.org>
2018-02-19 01:49:39 +0100

    update test

--- tests/phantomjsTest.js
+++ tests/phantomjsTest.js
@@ -2,17 +2,18 @@ var page = require('webpage').create();
 page.open('./tests/phantomTest/minimal.html',function(status){
 	if (status === "success") {
 		console.log("Started recording..");
+		var res;
 		setTimeout(function(){
-			page.evaluate(function(){
-				var res = document.defaultView.result;
-				if (res) {
-					console.log("Succesful! Execution order was according to the specifications. Exiting with 0!");
-					phantom.exit();
-				} else {
-					console.log("Execution order is mixed up. Specifications were not met! Exiting with 1!");
-					phantom.exit(1);
-				}
-			})
+			res = page.evaluate(function(){
+				return document.defaultView.result;
+			});
+			if (res) {
+				console.log("Succesful! Execution order was according to the specifications. Exiting with 0!");
+				phantom.exit();
+			} else {
+				console.log("Execution order is mixed up. Specifications were not met! Exiting with 1!");
+				phantom.exit(1);
+			}
 		},60000);
 	} else {
 		phantom.exit(1);
